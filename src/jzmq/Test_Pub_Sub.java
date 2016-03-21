0bf623c7f62f340c9803673e6918ba03b2f5e881
solq360 <solq360@126.com>
2016-03-21 15:58:01 +0800

    解决pub sub丢数据处理

--- src/jzmq/Test_Pub_Sub.java
+++ src/jzmq/Test_Pub_Sub.java
@@ -5,29 +5,27 @@ import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Socket;
 
 /***
- * @author solq 目前测试发送得太快会丢消息。。。。。。 TODO 未完成
+ * @author solq <br>
+ *         目前测试发送得太快会丢消息。。。。。。解决办法 发送一定频率sleep
  */
 public class Test_Pub_Sub extends TestCtx {
     public static void main(String[] args) throws InterruptedException {
 	final int count = 300000;
 	final Socket pub = ZMQ.context(1).socket(ZMQ.PUB);
 	pub.bind("tcp://*:5555");
-	//pub.setSndHWM(count);
- 	//System.out.println(pub.getSndHWM());
+	// pub.setSndHWM(count);
+	// System.out.println(pub.getSndHWM());
 	Thread t = new Thread(new Runnable() {
 	    public void run() {
 		final Socket sub = ZMQ.context(1).socket(ZMQ.SUB);
 		sub.connect("tcp://localhost:5555");
 		sub.subscribe("abc".getBytes());
-		//sub.setRcvHWM(count);
-		//System.out.println(sub.getRcvHWM());
-
 		int _count = count;
 		String body = null;
- 		while (_count-- > 0) {
+		while (--_count > 0) {
+		    body = new String(sub.recv(0));
 		    System.out.println(_count);
-		    body = new String(sub.recv(0)); 
- 		}
+		}
 		System.out.println(body);
 		sub.close();
 	    }
@@ -37,10 +35,9 @@ public class Test_Pub_Sub extends TestCtx {
 	long start = System.currentTimeMillis();
 	for (int i = 0; i < count; i++) {
 	    // TODO
-	    boolean flag = false;
-	    while (!flag) {
-		flag = pub.send("abc hello" + "_" + i);
-	    }
+	    pub.send("abc hello" + "_" + i);
+	    if (i % 1000 == 0)
+		Thread.sleep(10);
 	}
 	long end = System.currentTimeMillis();
 	System.out.println("push time :" + (end - start));
@@ -51,40 +48,61 @@ public class Test_Pub_Sub extends TestCtx {
     }
 
     @Test
-    public void myImpl() throws InterruptedException {
-	final Socket push = ZMQ.context(1).socket(ZMQ.PUSH);
-	final Socket pull = ZMQ.context(1).socket(ZMQ.PULL);
+    public void mutilSub() throws InterruptedException {
+	final int count = 100000;
+	int threadCount = Runtime.getRuntime().availableProcessors() * 50;
+	int client = threadCount;
+	final Socket pub = ZMQ.context(1).socket(ZMQ.PUB);
+	pub.bind("tcp://*:5555");
+	Thread[] threads = new Thread[threadCount];
 
-	push.bind("tcp://*:5555");
-	pull.connect("tcp://localhost:5555");
+	for (int i = 0; i < threadCount; i++) {
+	    threads[i] = new Thread(new Runnable() {
+		public void run() {
+		    final Socket sub = ZMQ.context(1).socket(ZMQ.SUB);
+		    sub.connect("tcp://localhost:5555");
+		    sub.subscribe("abc".getBytes());
+		    int _count = count;
+		    String body = null;
+		    long start = System.currentTimeMillis();
+		    boolean first = true;
+		    while (--_count > 0) {
+			body = new String(sub.recv(0));
+			if (first) {
+			    first = false;
+			    System.out.println(Thread.currentThread().getName() + ":" + body);
+			}
+		    }
+		    long end = System.currentTimeMillis();
+		    System.out.println(Thread.currentThread().getName() + ":" + (end - start));
 
-	final int count = 3000000;
-	Thread t = new Thread(new Runnable() {
-	    public void run() {
-		int v = 0;
-		String actual = null;
-		int _count = count;
-		while (_count-- > 0) {
-		    actual = new String(pull.recv());
-		    v++;
+		    sub.close();
 		}
-		pull.close();
-		System.out.println(actual);
-	    }
-	});
-	t.start();
+	    });
+	    threads[i].setDaemon(true);
+	    threads[i].start();
+	}
+	Thread.sleep(5000);
+	// 8个客户端 等侍15毫秒
+	int time = client / 8 * 15;
+	time = Math.max(15, time);
+	System.out.println("wait time : " + time);
 
-	final String expected = "hello";
 	long start = System.currentTimeMillis();
+
 	for (int i = 0; i < count; i++) {
-	    push.send(expected + "_" + i);
+	    // TODO
+	    pub.send("abc hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello" + "_" + i);
+	    if (i % 1000 == 0) {
+		Thread.sleep(time);
+	    }
 	}
 	long end = System.currentTimeMillis();
 	System.out.println("push time :" + (end - start));
 
-	t.join();
-	end = System.currentTimeMillis();
-	System.out.println("all time :" + (end - start));
+	for (int i = 0; i < threadCount; i++) {
+	    threads[i].join();
+	}
     }
 
 }
